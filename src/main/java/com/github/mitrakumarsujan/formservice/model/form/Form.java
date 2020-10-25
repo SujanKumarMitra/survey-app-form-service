9790c5d0ee9a149d0f20ae7d9c83f4122a6909a8
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-10-25 19:41:07 +0530

    added json property order

--- src/main/java/com/github/mitrakumarsujan/formservice/model/form/Form.java
+++ src/main/java/com/github/mitrakumarsujan/formservice/model/form/Form.java
@@ -2,10 +2,13 @@ package com.github.mitrakumarsujan.formservice.model.form;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 /**
  * @author Sujan Kumar Mitra
  * @since 2020-10-25
  */
+@JsonPropertyOrder({ "uid", "key", "template" })
 public interface Form extends Serializable {
 
 	String getUID();
