4d9fd8a7d91b588de37fdb882b32b85b3b81e358
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-10-26 11:18:09 +0530

    added component scan config

---
+++ src/main/java/com/github/mitrakumarsujan/formservice/configuration/ComponentScanConfiguration.java
@@ -0,0 +1,13 @@
+package com.github.mitrakumarsujan.formservice.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Sujan Kumar Mitra
+ * @since 2020-10-26
+ */
+@Configuration
+@ComponentScan(basePackages = { "com.github.mitrakumarsujan" })
+public class ComponentScanConfiguration {
+}
